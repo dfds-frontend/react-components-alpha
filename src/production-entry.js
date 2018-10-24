55ca2189fba38eae1e7ac43f0dbafecc9e268a3c
Kunuk Nykjær <kunyk@dfds.com>
2018-10-24 22:56:04 +0200

    explicitly load components for build

--- src/production-entry.js
+++ src/production-entry.js
@@ -2,9 +2,13 @@ import 'components/quarks/_main.scss';
 import '@dfds-frontend/fonts/main/font.css';
 import icons from '@dfds-frontend/icons';
 
-import { DataLoader, Button, Icon } from 'components';
+import DataLoader from 'components/atoms/DataLoader/DataLoader';
+import Icon from 'components/atoms/Icon/Icon';
+import Button  from 'components/molecules/Button/Button';
 
 const loadIcons = icons.load;
+
+//console.log(!!loadIcons, !!Button, !!Icon, !!DataLoader);
 
 export {
     loadIcons,
