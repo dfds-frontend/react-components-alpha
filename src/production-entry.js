e3a67b82736de471af294355c85425553ad1796b
Kunuk Nykjær <kunyk@dfds.com>
2018-10-24 23:14:58 +0200

    upd font dependency

--- src/production-entry.js
+++ src/production-entry.js
@@ -1,10 +1,10 @@
 import 'components/quarks/_main.scss';
-import '@dfds-frontend/fonts/main/font.css';
+//import '@dfds-frontend/fonts/main/font.css'; // does not work
 import icons from '@dfds-frontend/icons';
 
 import DataLoader from 'components/atoms/DataLoader/DataLoader';
 import Icon from 'components/atoms/Icon/Icon';
-import Button  from 'components/molecules/Button/Button';
+import Button from 'components/molecules/Button/Button';
 
 const loadIcons = icons.load;
 
