526aabe90cd59461a7701134cf60ec3f63ae895e
Kunuk Nykjær <kunyk@dfds.com>
2018-11-09 14:24:03 +0100

    upd post json method

--- src/utils/postJsonAsync.js
+++ src/utils/postJsonAsync.js
@@ -1,21 +1,19 @@
 export default async function postJsonAsync(url, options) {
-    const response = await fetch(url, {
-        method: 'post',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(options),
-    });
+  const response = await fetch(url, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(options),
+  });
 
-    const contentType = response.headers.get('content-type');
+  let data = null;
+  let text = await response.text();
 
-    let data;
-    if (contentType && contentType.indexOf('application/json') !== -1) {
-        data = await response.json();
-    } else {
-        data = await response.text();
-    }
+  if (text) {
+    data = JSON.parse(text);
+  }
 
-    return { data, status: response.status };
+  return { data, status: response.status };
 }
